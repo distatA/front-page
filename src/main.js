f2be610fc9ebc38a8c6b043ee4026c83aa3c9d13
Yoo-G <Q245063558@163.com>
2020-03-24 15:37:20 +0800

    清空配置

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+// vue构造函数
+import Vue from "vue";
+// app组件(以.vue结尾的文件都是组件)
+import App from "./App.vue";
+// 路由对象
+import router from "./router";
+// 上线环境是否提示信息(可以忽略)
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
-
+// 创建一个vue根实例
+//.$mount('#app') 相当于el配置,指定id为app的元素作为模版
 new Vue({
+  // 路由对象
   router,
+  // 加载第一个子组件,最底层的组件,(写法是固定的)
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
